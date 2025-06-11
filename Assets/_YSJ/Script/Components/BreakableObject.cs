e90a9762ca5131dc70eb2593ecaa27db22fa83b2
seoungjun <junyoon99@naver.com>
2025-06-11 21:28:42 +0900

    [MOD] 파괴가능 오브젝트 TakeDamage에도 부서지게 수정

--- Assets/_YSJ/Script/Components/BreakableObject.cs
+++ Assets/_YSJ/Script/Components/BreakableObject.cs
@@ -2,9 +2,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using YSJ;
 
-public class BreakableObject : MonoBehaviour
+public class BreakableObject : MonoBehaviour, IDamagable
 {
     [SerializeField] GameObject Parts;
+
+    public void TakeDamage(float damage)
+    {
+        Break();
+    }
 
     void Awake()
     {
@@ -14,19 +19,41 @@ public class BreakableObject : MonoBehaviour
     {
         if (collision.GetComponent<Rigidbody2D>())
         {
-            GameObject spawnedParts = Instantiate(Parts, transform.position, transform.rotation);
-            spawnedParts.transform.SetParent(transform.parent, true);
-
-            foreach (Transform part in spawnedParts.transform)
-            {
-                Rigidbody2D rb2d = part.GetComponent<Rigidbody2D>();
-                Vector2 direction = (rb2d.position - (Vector2)collision.transform.position).normalized;
-                rb2d.AddForce(direction * Random.Range(3f, 10f), ForceMode2D.Impulse);
-
-                rb2d.AddTorque(Random.Range(-1f, 1f), ForceMode2D.Impulse);
-            }
-
-            Destroy(gameObject);
+            Break(collision);
         }
+    }
+
+    void Break(Collider2D collision) 
+    {
+        GameObject spawnedParts = Instantiate(Parts, transform.position, transform.rotation);
+        spawnedParts.transform.SetParent(transform.parent, true);
+
+        foreach (Transform part in spawnedParts.transform)
+        {
+            Rigidbody2D rb2d = part.GetComponent<Rigidbody2D>();
+            Vector2 direction = (rb2d.position - (Vector2)collision.transform.position).normalized;
+            rb2d.AddForce(direction * Random.Range(3f, 10f), ForceMode2D.Impulse);
+
+            rb2d.AddTorque(Random.Range(-1f, 1f), ForceMode2D.Impulse);
+        }
+
+        Destroy(gameObject);
+    }
+
+    void Break() 
+    {
+        GameObject spawnedParts = Instantiate(Parts, transform.position, transform.rotation);
+        spawnedParts.transform.SetParent(transform.parent, true);
+
+        foreach (Transform part in spawnedParts.transform)
+        {
+            Rigidbody2D rb2d = part.GetComponent<Rigidbody2D>();
+            Vector2 direction = new Vector2(Random.Range(-1, 1), Random.Range(-1, 1)).normalized;
+            rb2d.AddForce(direction * Random.Range(3f, 10f), ForceMode2D.Impulse);
+
+            rb2d.AddTorque(Random.Range(-1f, 1f), ForceMode2D.Impulse);
+        }
+
+        Destroy(gameObject);
     }
 }
