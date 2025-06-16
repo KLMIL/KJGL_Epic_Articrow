aba38fe7d1ab36815f775560e6f574b257beea91
kyeongtae-git <choikt9012@gmail.com>
2025-06-16 13:43:04 +0900

    [FIX] 아티팩트 성능 수정

--- Assets/_CKT/Scripts/Items/Prefabs/GrabObject.cs
+++ Assets/_CKT/Scripts/Items/Prefabs/GrabObject.cs
@@ -5,19 +5,20 @@ namespace CKT
 {
     public class GrabObject : MonoBehaviour
     {
-        float _minSqrDistance = 2f;
-        float _moveSpeed = 10f;
-        LayerMask _playerLayerMask;
+        int _level = 0;
+        float _grabScale = 1f;
+        LayerMask _ignoreLayerMask;
 
         Transform _target;
 
         private void OnEnable()
         {
-            _playerLayerMask = LayerMask.GetMask("Player");
+            _ignoreLayerMask = LayerMask.GetMask("Default", "Ignore Raycast", "Player", "BreakParts");
         }
 
         private void OnDisable()
         {
+            _level = 0;
             _target = null;
         }
 
@@ -31,7 +32,7 @@ namespace CKT
 
         void Grab()
         {
-            RaycastHit2D[] hits = Physics2D.CircleCastAll(this.transform.position, this.transform.localScale.x, Vector2.up, 0, ~_playerLayerMask);
+            RaycastHit2D[] hits = Physics2D.CircleCastAll(this.transform.position, this.transform.localScale.x, Vector2.up, 0, ~_ignoreLayerMask);
             for (int i = 0; i < hits.Length; i++)
             {
                 IDamagable iDamagable = hits[i].transform.GetComponent<IDamagable>();
@@ -58,11 +59,13 @@ namespace CKT
 
             if ((player != null) && (target != null))
             {
+                float distance = 0;
                 float sqrDistance = float.MaxValue;
-                while (sqrDistance > _minSqrDistance)
+                while (distance < _grabScale * _level)
                 {
+                    distance += Time.deltaTime;
                     Vector3 moveDir = (playerPos - startPos).normalized;
-                    transform.position += moveDir * _moveSpeed * Time.deltaTime;
+                    transform.position += moveDir * _grabScale * Time.deltaTime;
                     sqrDistance = (playerPos - this.transform.position).sqrMagnitude;
                     yield return null;
                 }
