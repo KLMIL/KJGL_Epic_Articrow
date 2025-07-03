ce458c799b46132e9e9b3ef415e33ff5044c90c2
bokob <bmc0520@naver.com>
2025-07-03 13:05:41 +0900

    [FIX] 마나 반 칸 지원하도록 수정

--- Assets/_BMC/Scripts/UI/ManaHeart.cs
+++ Assets/_BMC/Scripts/UI/ManaHeart.cs
@@ -4,12 +4,13 @@ using UnityEngine.UI;
 public enum ManaStatus
 {
     Empty,
+    Half,
     Full
 }
 
 public class ManaHeart : MonoBehaviour
 {
-    public Sprite fullMana, emptyMana;
+    public Sprite fullMana, halfMana, emptyMana;
     Image ManaImage;
 
     private void Awake()
@@ -23,6 +24,9 @@ public class ManaHeart : MonoBehaviour
         {
             case ManaStatus.Empty:
                 ManaImage.sprite = emptyMana;
+                break;
+            case ManaStatus.Half:
+                ManaImage.sprite = halfMana;
                 break;
             case ManaStatus.Full:
                 ManaImage.sprite = fullMana;
