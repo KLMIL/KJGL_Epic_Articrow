c7c40f74268387c295ca52944d187abdf63e18f5
bokob <bmc0520@naver.com>
2025-07-03 21:42:45 +0900

    [FIX] 빌드 실패하게 하는 네임스페이스 삭제

--- Assets/_YSJ/Script/MagicAttack/MagicFluteSkillAttackCircle_YSJ.cs
+++ Assets/_YSJ/Script/MagicAttack/MagicFluteSkillAttackCircle_YSJ.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Localization.Plugins.XLIFF.V20;
 using UnityEngine;
 
 public class MagicFluteSkillAttackCircle_YSJ : MagicRoot_YSJ
