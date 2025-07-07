0ffa426cd02fe1445e63e8d99f239a9855b98633
bokob <bmc0520@naver.com>
2025-07-08 02:20:43 +0900

    [FIX] 인게임 내에서 사망 시, 재시작하면 시작방으로 전환

--- Assets/_BMC/Scripts/UI/InGameScene/RetryBtn.cs
+++ Assets/_BMC/Scripts/UI/InGameScene/RetryBtn.cs
@@ -17,7 +17,7 @@ namespace BMC
         void OnClicked()
         {
             PlayerManager.Instance.Clear();
-            SceneManager.LoadScene(2);
+            YSJ.Managers.Scene.LoadScene("StageStartScene");
         }
     }
 }
