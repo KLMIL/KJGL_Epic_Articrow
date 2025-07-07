0d8719b4b02ab0ebdab0b313322296f08213cd56
bokob <bmc0520@naver.com>
2025-07-08 02:07:52 +0900

    [FIX] 돌진 인디케이터 화살표로 교체

--- Assets/_BMC/Behavior/Boss/Stage1/Indicator/RushAttackIndicator.cs
+++ Assets/_BMC/Behavior/Boss/Stage1/Indicator/RushAttackIndicator.cs
@@ -33,37 +33,21 @@ namespace BMC
             transform.right = dir;
 
             // 원래 스케일 초기화
-            _indicator.transform.localScale = new Vector3(0, _startScale, 1);
             _background.transform.localScale = new Vector3(0, _endScale, 1);
             _indicator.enabled = true;
-            _background.enabled = true;
 
-            //_dist = Vector2.Distance(_indicator.transform.position, _target.position) / 2;
-
-            // 1. 길이 확장
-            _indicator.transform.localScale = new Vector3(_dist, _endScale, 1);
-            _background.transform.localScale = new Vector3(_dist, _endScale, 1);
-
-            // 2. 배경에 딱 채우기
             float timer = 0;
             while (timer < _fillDuration)
             {
                 timer += Time.deltaTime;
                 float r = timer / _fillDuration;
                 float localScaleY = Mathf.Lerp(_startScale, _endScale, r);
-                _indicator.transform.localScale = new Vector3(_dist, localScaleY, 1);
                 yield return null;
             }
-            _indicator.transform.localScale = new Vector3(_dist, _endScale, 1);
 
             _indicator.enabled = false;
-            _background.enabled = false;
             yield return new WaitForSeconds(0.1f);
             _coroutine = null;
-
-            // 초기화
-            _indicator.transform.localScale = new Vector3(0, _startScale, 1);
-            _background.transform.localScale = new Vector3(0, _endScale, 1);
         }
     }
 }
