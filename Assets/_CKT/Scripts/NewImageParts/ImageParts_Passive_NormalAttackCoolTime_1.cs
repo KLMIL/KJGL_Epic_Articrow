c4ed54ced4cd6af5179baf11ddc09fe280bf8181
kyeongtae-git <choikt9012@gmail.com>
2025-07-10 20:30:22 +0900

    [FIX] 아티팩트, 스킬파츠 밸런스 조정
    1. 아티팩트 스탯 표에 맞게 수정
    2. 스킬 파츠 쿨타임 관련 고정값으로 변경
    3. 데미지 표기 무한 소수 문제 수정
    4. 변경 사항에 맞춰 LocalizationTable 수정

--- Assets/_CKT/Scripts/NewImageParts/ImageParts_Passive_NormalAttackCoolTime_1.cs
+++ Assets/_CKT/Scripts/NewImageParts/ImageParts_Passive_NormalAttackCoolTime_1.cs
@@ -1,22 +1,25 @@
 using UnityEngine;
 
 /// <summary>
-/// 일반 공격 쿨타임 20% 감소
+/// 일반 공격 쿨타임 0.05초 감소 (최소 쿨타임 0.1초)
 /// </summary>
 public class ImageParts_Passive_NormalAttackCoolTime_1 : ImagePartsRoot_YSJ, IImagePartsToNormalAttack_YSJ
 {
-    float _percent = 20f;
+    float _percent = 30f;
+    float _coolTimeValue = 0.05f;
 
     public override string partsName => "Passive_NormalAttackCoolTime_1";
 
     #region [Normal]
     public void NormalAttackPessive(Artifact_YSJ fireArtifact)
     {
-        float add = _percent * fireArtifact.normalStatus.Current_AttackCoolTime;
+        /*float add = _percent * fireArtifact.normalStatus.Current_AttackCoolTime;
         add = 0.01f * Mathf.RoundToInt(add);
 
         fireArtifact.normalStatus.Added_AttackCoolTime -= add;
-        Debug.Log($"[ckt] {partsName} {add}");
+        Debug.Log($"[ckt] {partsName} {add}");*/
+        fireArtifact.normalStatus.Added_AttackCoolTime -= _coolTimeValue;
+        Debug.Log($"[ckt] {partsName} {_coolTimeValue}");
     }
 
     public void NormalAttackBeforeFire(Artifact_YSJ fireArtifact)
