caeefad3e7cd536da25fa5eb28ae84c7623e25ca
kyeongtae-git <choikt9012@gmail.com>
2025-07-10 13:49:21 +0900

    [FIX] Artifact_Flute 사거리 로직 수정

--- Assets/_YSJ/Script/MagicAttack/MagicFluteSkillAttack_YSJ.cs
+++ Assets/_YSJ/Script/MagicAttack/MagicFluteSkillAttack_YSJ.cs
@@ -26,6 +26,8 @@ public class MagicFluteSkillAttack_YSJ : MagicRoot_YSJ
         {
             // 시간기록 초기화
             elapsedTime = 0;
+            //오브젝트 크기 설정
+            transform.GetChild(attackIndex).transform.localScale = Vector3.one * base.LifeTime * base.Speed * Mathf.Pow(1.5f, attackIndex);
             // 오브젝트 활성화 시켜서 공격
             transform.GetChild(attackIndex).gameObject.SetActive(true);
             // 인덱스 한칸 옮기기
