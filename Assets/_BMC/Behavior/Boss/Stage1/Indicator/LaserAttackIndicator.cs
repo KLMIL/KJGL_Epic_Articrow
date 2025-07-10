30ae479e250c0c9829787fead5d3db71992100d5
bokob <bmc0520@naver.com>
2025-07-10 21:46:10 +0900

    [FIX] 보스 3연속 레이저 공격 외형 변경

--- Assets/_BMC/Behavior/Boss/Stage1/Indicator/LaserAttackIndicator.cs
+++ Assets/_BMC/Behavior/Boss/Stage1/Indicator/LaserAttackIndicator.cs
@@ -15,6 +15,10 @@ namespace BMC
         [Header("시간")]
         float _attackTime = 0.5f;   // 공격 지속 시간
         int _loopCount = 3;         // 반복 횟수
+        float _tempFillDuration;    // 초기 확장 시간
+        float _secondFillDuration = 0.25f;
+
+        [SerializeField] GameObject _laserPrefab;
 
         void Awake()
         {
@@ -36,6 +40,7 @@ namespace BMC
             if (_target == null)
                 yield break;
 
+            _tempFillDuration = _fillDuration;
             for (int i = 0; i < _loopCount; i++)
             {
                 // 목표를 바라보게 회전
@@ -56,10 +61,10 @@ namespace BMC
 
                 // 2. 배경에 딱 채우기
                 float timer = 0;
-                while (timer < _fillDuration)
+                while (timer < _tempFillDuration)
                 {
                     timer += Time.deltaTime;
-                    float r = timer / _fillDuration;
+                    float r = timer / _tempFillDuration;
                     float localScaleY = Mathf.Lerp(_startScale, _endScale, r);
                     _indicator.transform.localScale = new Vector3(_dist, localScaleY, 1);
                     yield return null;
@@ -69,22 +74,25 @@ namespace BMC
                 // 공격 실행
                 Managers.Sound.PlaySFX(Define.SFX.GolemLaser);
                 _background.enabled = false;
-                _indicator.enabled = true;
+                _indicator.enabled = false;
                 _boxCollider.enabled = true;
                 Color originalColor = _indicator.color;
-                _indicator.color = Color.white;
+                //_indicator.color = Color.;
 
+                // 레이저 스폰
+                GameObject laserInstance = Instantiate(_laserPrefab, _indicator.transform.position, Quaternion.identity);
+                laserInstance.transform.right = dir;
                 yield return new WaitForSeconds(_attackTime);
 
                 _boxCollider.enabled = false;
                 _indicator.enabled = false;
                 _indicator.color = originalColor;
-                _coroutine = null;
-
+                
                 _indicator.transform.localScale = new Vector3(0, _startScale, 1);
                 _background.transform.localScale = new Vector3(0, _endScale, 1);
-                _fillDuration = 0.25F;
+                _tempFillDuration = _secondFillDuration;
             }
+            _coroutine = null;
         }
     }
 }
