5c1e1b204040b35bc3275a4603301f11e0ae9475
kyeongtae-git <choikt9012@gmail.com>
2025-07-12 11:34:36 +0900

    [FIX] 파츠 밸런스 조정, 설명창 크기 위치 조정
    1. Passive_SkillAttackPower_1 스킬 시전 시간 증가 20% -> 0.05초
    2. Description Prefab width 440 -> 500, position -230 -> 250

--- Assets/_CKT/Scripts/NewImageParts/ImageParts_Passive_SkillAttackpower_1.cs
+++ Assets/_CKT/Scripts/NewImageParts/ImageParts_Passive_SkillAttackpower_1.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 
 /// <summary>
-/// 스킬 시전 시간 20% 증가, 스킬 피해량 20% 증가
+/// 스킬 시전 시간 0.05초 증가, 스킬 피해량 20% 증가
 /// </summary>
 public class ImageParts_Passive_SkillAttackPower_1 : ImagePartsRoot_YSJ, IImagePartsToSkillAttack_YSJ
 {
-    float _delayPercent = 20f;
+    //float _delayPercent = 20f;
+    float _delayValue = 0.05f;
     float _powerPercent = 20f;
 
     public override string partsName => "Passive_SkillAttackPower_1";
@@ -13,14 +14,14 @@ public class ImageParts_Passive_SkillAttackPower_1 : ImagePartsRoot_YSJ, IImageP
     #region [Skill]
     public void SkillAttackPessive(Artifact_YSJ fireArtifact)
     {
-        float addDelay = _delayPercent;
-        addDelay = 0.01f * Mathf.RoundToInt(addDelay);
+        //float addDelay = _delayPercent;
+        //addDelay = 0.01f * Mathf.RoundToInt(addDelay);
         float addPower = _powerPercent * fireArtifact.skillStatus.Default_AttackPower;
         addPower = 0.01f * Mathf.RoundToInt(addPower);
 
-        fireArtifact.skillStatus.Added_AttackStartDelay += addDelay;
+        fireArtifact.skillStatus.Added_AttackStartDelay += _delayValue;
         fireArtifact.skillStatus.Added_AttackPower += addPower;
-        Debug.Log($"[ckt] {partsName} {addDelay}_{addPower}");
+        Debug.Log($"[ckt] {partsName} {fireArtifact.skillStatus.Current_AttackStartDelay}_{_delayValue}_{addPower}");
     }
 
     public void SkillAttackBeforeFire(Artifact_YSJ fireArtifact)
