8e90d70d00a4e177225ba301a5f5f61e4494ac74
KLMIL <kanoline@naver.com>
2025-07-12 13:56:56 +0900

    feat: 다음 룸 넘어갈 때 진행 이미지 추가

--- Assets/Scripts/Managers/SceneManagerEx.cs
+++ Assets/Scripts/Managers/SceneManagerEx.cs
@@ -1,4 +1,5 @@
 using BMC;
+using Game.Dungeon;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -49,23 +50,49 @@ public class SceneManagerEx
         }
     }
 
-    public IEnumerator LoadSceneCoroutine(string sceneName)
+    public IEnumerator LoadSceneCoroutine(string sceneName, float duration = 2f)
     {
-        //Debug.Log("왜 안돼");
-
+        // 1. FadeOut 시작
         UI_SceneTransitionCanvas sceneFade = GameObject.FindAnyObjectByType<UI_SceneTransitionCanvas>();
         yield return sceneFade.FadeOutCoroutine(1f); // 페이드 아웃
 
+
+        // 2. 진행상황 UI 활성화
+        UI_StageProgressCanvas stageProgress = GameObject.FindAnyObjectByType<UI_StageProgressCanvas>();
+        stageProgress.Show(GameFlowManager.Instance.CurrentRoom - 1, GameFlowManager.Instance.CurrentRoom);
+
+
+        // 3. 씬 비동기 로드 시작 (즉시 활성화 하지 않음)
         AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneName);
         asyncOperation.allowSceneActivation = false; // 씬 활성화는 나중에
+
+
+        // 4. 연출 애니메이션 실행
+        yield return stageProgress.PlayerProgressAnimation();
+        yield return new WaitForSecondsRealtime(0.3f);
+
+
+        // 5. 씬 비동기 로딩이 끝날 때까지 대기(로딩이 느린 경우 대비)
+        while (asyncOperation.progress < 0.9f)
+        {
+            yield return null;
+        }
+
+
+        // 6. 씬 활성화
+        asyncOperation.allowSceneActivation = true;
         while (!asyncOperation.isDone)
         {
-            if (asyncOperation.progress >= 0.9f) // 로딩이 거의 끝났을 때
-            {
-                asyncOperation.allowSceneActivation = true; // 씬 활성화
-            }
-            yield return null; // 다음 프레임까지 대기
+            yield return null;
         }
+
+
+        // 7. 진행상황 UI 비활성화
+        stageProgress.Hide();
+
+
+        // 8. FadeIn 시작
+        yield return sceneFade.FadeInCoroutine(0.5f);
     }
 
     // 씬 정리
