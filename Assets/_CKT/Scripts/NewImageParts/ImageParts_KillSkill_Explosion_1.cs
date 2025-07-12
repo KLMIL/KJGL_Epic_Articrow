5d427e6279c8afc7e709b82c5e0de6e215f43491
kyeongtae-git <choikt9012@gmail.com>
2025-07-12 11:09:34 +0900

    [FIX] KillSkill_Explosion 계수 150%로 조정
    1. 250% -> 150%

--- Assets/_CKT/Scripts/NewImageParts/ImageParts_KillSkill_Explosion_1.cs
+++ Assets/_CKT/Scripts/NewImageParts/ImageParts_KillSkill_Explosion_1.cs
@@ -2,11 +2,11 @@ using Game.Enemy;
 using UnityEngine;
 
 /// <summary>
-/// 스킬로 대상을 처치했을 때 현재 공격의 250%에 해당하는 광역 공격 생성
+/// 스킬로 대상을 처치했을 때 현재 공격의 150%에 해당하는 광역 공격 생성
 /// </summary>
 public class ImageParts_KillSkill_Explosion_1 : ImagePartsRoot_YSJ, IImagePartsToSkillAttack_YSJ
 {
-    float _damagePercent = 250f;
+    float _damagePercent = 150f;
 
     public override string partsName => "KillSkill_Explosion_1";
 
@@ -29,24 +29,30 @@ public class ImageParts_KillSkill_Explosion_1 : ImagePartsRoot_YSJ, IImagePartsT
 
     public void SKillAttackOnHit(Artifact_YSJ fireArtifact, GameObject spawnedAttack, GameObject hitObject)
     {
-        EnemyController enemyController = hitObject.transform.root.GetComponentInChildren<EnemyController>();
-        if (enemyController == null)
-            return;
+        EnemyController enemy = hitObject.transform.GetComponentInParent<EnemyController>();
+        MagicRoot_YSJ magic = spawnedAttack.GetComponent<MagicRoot_YSJ>();
 
-        float enemyHealth = enemyController.Status.healthPoint;
-        float attackPower = spawnedAttack.GetComponent<MagicRoot_YSJ>().AttackPower;
-        Debug.Log($"[ckt] {partsName} enemyHealth:{enemyHealth}, attackPower:{attackPower}");
-        if (enemyHealth <= attackPower)
+        if ((enemy != null) && (magic != null))
         {
-            //폭발 생성
-            GameObject explosion = YSJ.Managers.Pool.Get<GameObject>(Define.PoolID.HitExplosion);
-            explosion.transform.position = hitObject.transform.position;
+            float enemyHealth = enemy.Status.healthPoint;
+            float attackPower = magic.AttackPower;
+            //Debug.Log($"[ckt] {partsName} enemyHealth:{enemyHealth}, attackPower:{attackPower}");
+            if (enemyHealth <= attackPower)
+            {
+                //폭발 생성
+                GameObject explosion = YSJ.Managers.Pool.Get<GameObject>(Define.PoolID.HitExplosion);
+                explosion.transform.position = hitObject.transform.position;
 
-            float damage = _damagePercent * spawnedAttack.GetComponent<MagicRoot_YSJ>().AttackPower;
-            damage = 0.01f * Mathf.RoundToInt(damage);
+                float damage = _damagePercent * spawnedAttack.GetComponent<MagicRoot_YSJ>().AttackPower;
+                damage = 0.01f * Mathf.RoundToInt(damage);
 
-            explosion.GetComponent<CKT.Explosion>().Init(damage);
-            Debug.Log($"[ckt] {partsName} KillExplosion {damage}");
+                explosion.GetComponent<CKT.Explosion>().Init(damage);
+                Debug.Log($"[ckt] {partsName} KillExplosion {damage}");
+            }
+        }
+        else
+        {
+            Debug.LogWarning($"[ckt] {partsName} EnemyController or MagicRoot_YSJ is null");
         }
     }
     #endregion
