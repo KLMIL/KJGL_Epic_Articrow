e75017a65b55152af64c7a825ee559a73770ef19
seoungjun <junyoon99@naver.com>
2025-07-16 16:20:53 +0900

    Reapply "[FIX] 죽었을 때 발사 가능, 회전 가능 문제 수정"
    This reverts commit 2a6abd522db39b9f2391e5a06417e71c1bb82191.

--- Assets/Scripts/Player/Hand.cs
+++ Assets/Scripts/Player/Hand.cs
@@ -1,3 +1,4 @@
+using BMC;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -59,6 +60,11 @@ namespace YSJ
 
         protected void SpriteRotation()
         {
+            // 손을 회전 못하는 경우거나 플레이어가 죽어있는 경우면 회전하지 않음
+            if ((_hand && !_hand.CanHandling) || PlayerManager.Instance.PlayerHurt.IsDead)
+            {
+                return;
+            }
             Vector2 mousePos = Managers.Input.MouseWorldPos;
             Vector2 mouseDir = (mousePos - (Vector2)this.transform.position).normalized;
             float angleZ = Mathf.Atan2(mouseDir.y, mouseDir.x) * Mathf.Rad2Deg;
--- Assets/Scripts/Player/RightHand.cs
+++ Assets/Scripts/Player/RightHand.cs
@@ -100,14 +100,6 @@ namespace YSJ
         }
 
         #region 통계 관련
-        void Update()
-        {
-            if(Input.GetKeyDown(KeyCode.Y))
-            {
-                RecordEquipParts();
-            }
-        }
-
         // 아티팩트에 장착된 부품 기록
         public void RecordEquipParts()
         {
