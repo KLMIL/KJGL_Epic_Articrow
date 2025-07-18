56da0f1a963bc494e29d0b993b0861c2c87ab100
bokob <bmc0520@naver.com>
2025-07-18 13:48:15 +0900

    [FEAT] 중복 키 입력 시, 안내 문구 출력

--- Assets/_BMC/Scripts/UI/Common/Settings/Controls/KeyConfirmationCanvas.cs
+++ Assets/_BMC/Scripts/UI/Common/Settings/Controls/KeyConfirmationCanvas.cs
@@ -1,19 +1,34 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.Localization.Settings;
 using YSJ;
 
 public class KeyConfirmationCanvas : MonoBehaviour
 {
+    #region Localization
+    static readonly string SettingsControlsTable = "SettingsControlsTable";
+    #endregion
+
     public Canvas Canvas;
+    public TextMeshProUGUI GuidText;
     void Awake()
     {
         Canvas = GetComponent<Canvas>();
+        GuidText = GetComponentInChildren<TextMeshProUGUI>();
         //UI_TitleEventBus.OnActiveKeyConfirmationCanvas = ToggleCanvas;
         UI_CommonEventBus.OnActiveKeyConfirmationCanvas = ToggleCanvas;
         Managers.UI.KeyConfirmationCanvas = this;
+        SetGuidText("PressKey");
     }
 
     void ToggleCanvas(bool isActive)
     {
         Canvas.enabled = isActive;
+    }
+
+    public void SetGuidText(string localizationKey)
+    {
+        string localeDescription = LocalizationSettings.StringDatabase.GetLocalizedString(SettingsControlsTable, localizationKey, LocalizationSettings.SelectedLocale);
+        GuidText.text = localeDescription;
     }
 }
