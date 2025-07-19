ac3a0eedfbe30bca29329a0f3f0aed1e41f72b32
bokob <bmc0520@naver.com>
2025-07-19 16:39:27 +0900

    [FEAT] 플레이 데이터 수집 동의 UI 초기 구조

---
+++ Assets/Scripts/Analytics/PrivacyPolicy/InformationCollectionAgreeBtn.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InformationCollectionAgreeBtn : MonoBehaviour
+{
+    Button _btn;
+    Canvas _canvas;
+
+    void Awake()
+    {
+        _btn = GetComponent<Button>();
+        _canvas = GetComponentInParent<Canvas>();
+        _btn.onClick.AddListener(OnClicked);
+    }
+
+    void OnClicked()
+    {
+        PlayerPrefs.SetInt("InformationCollection", 1);
+        PlayerPrefs.Save();
+        Debug.Log("플레이 데이터 수집 동의");
+
+        // TODO: 통계 분석 시스템 활성화
+
+        _canvas.gameObject.SetActive(false);
+    }
+}
